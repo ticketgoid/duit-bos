3b2a78b65eb4a90a3483dae372b274273159d5a9
TicketGo <ticketgo.ticketing.id@gmail.com>
2026-03-24 15:48:23 +0700

    final check

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:duit_bos/main.dart';
-
-void main() {
-  testWidgets('App smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const ProviderScope(child: DuitBosApp()));
-  });
-}
